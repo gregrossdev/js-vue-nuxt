54acddc4d685310778777cc3e3ca6ec4347cefdd
Gregory Ross <gregross.dev@gmail.com>
2022-10-24 11:46:03 -0400

    NuxtLink nav component for layout

---
+++ 00-basics/components/layouts/navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/about">About</NuxtLink>
+  </nav>
+</template>
+
+<style>
+  nav {
+    display: flex;
+  }
+
+  nav a {
+    padding: 0.25rem;
+  }
+</style>
--- 00-basics/layouts/default.vue
+++ 00-basics/layouts/default.vue
@@ -1,10 +1,11 @@
 <script setup>
-
+import Navbar from '../components/layouts/navbar';
 </script>
 
 <template>
   <div class="wrapper">
     <h1>00-basics</h1>
+    <Navbar />
     <slot />
   </div>
 </template>
