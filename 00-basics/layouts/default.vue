643d38520634cf29ced51db71f8ddc8d15423510
Gregory Ross <gregross.dev@gmail.com>
2022-10-24 11:38:03 -0400

    default layout

---
+++ 00-basics/layouts/default.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>00-basics</h1>
+    <slot />
+  </div>
+</template>
---
+++ 00-basics/pages/index.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+  <h2>pages</h2>
+</template>
